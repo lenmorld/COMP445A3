ddf91e3c46d7acc9453010d8f9c045284eb4022b
Lenmor LD <lenmorld@live.com>
2016-11-17 22:51:05 -0500

    handshake done

--- python/udp_client.py
+++ python/udp_client.py
@@ -1,6 +1,7 @@
 import argparse
 import ipaddress
 import socket
+import json
 
 from packet import Packet
 import sys
@@ -29,97 +30,132 @@ NAK = -1
 
 
 
-def three_way_handshake(router_addr, router_port, server_addr, server_port):
+def three_way_handshake(router_addr, router_port, server_addr, server_port, conn):
     peer_ip = ipaddress.ip_address(socket.gethostbyname(server_addr))
-    
-    conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    timeout = 5    
 
+    # ---> just use conn from run_client, we won't create seperate conn for handshake
+    # conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # timeout = 5    
 
-    my_seq_num = 100
+    initial_seq_num = 100
 
     try:
+
+        # HANDSHAKE step 1: create SYN packet
+        # TODO: implement timeout for this
+
         msg = ""   # no payload in handshake
         p = Packet(packet_type=1,
-                   seq_num=my_seq_num,
+                   seq_num=initial_seq_num,
                    peer_ip_addr=peer_ip,
                    peer_port=server_port,
                    payload=msg.encode("utf-8"))
 
-        # 1st step of handshake : send SYN
+        # send SYN packet
         conn.sendto(p.to_bytes(), (router_addr, router_port))
-        print('Send "{}" to router'.format(msg))
+        # print('Send "{}" to router'.format(msg))
 
         # wait for SYN-ACK
+
+        # 
         # Try to receive a response within timeout
-        conn.settimeout(timeout)
-        print('SYN with SEQ ', my_seq_num, ' sent ')
+        # conn.settimeout(timeout)
+
+        print('Sending SYN with SEQ ', initial_seq_num)
         print('Waiting for a response...')
         
         response, sender = conn.recvfrom(1024)
         handshake_packet = Packet.from_bytes(response)
+        hs_payload = handshake_packet.payload.decode("utf-8")
 
         packet_type = handshake_packet.packet_type
-        seq_num = handshake_packet.seq_num
+        his_seq_num = handshake_packet.seq_num
+        # get ack from payload
+        dict_payload = json.loads(hs_payload)
+        his_ack = dict_payload['ack']
+
 
         # check packet type
-        if packet_type == SYN_ACK:   # 3rd step of handshake: server sent SYN_ACK, send final ACK
-            # if SYN, this is step 1 of handshake, add 1 to client's seq_num
-            my_ack = seq_num + 1
-            # generate my own seq_num
-            my_seq_num = my_seq_num + 1    # increment my seq num
+        if packet_type == SYN_ACK:   
 
-            # create final ACK packet
-            msg = ""
-            ack_p = Packet(packet_type=2,
-                   seq_num=my_seq_num,
-                   peer_ip_addr=peer_ip,
-                   peer_port=server_port,
-                   payload=msg.encode("utf-8"))
+            print("received SYN_ACK from server, ACK:", his_ack, " SEQ:", his_seq_num )
 
-            conn.sendto(ack_p.to_bytes(), sender)
-            print("ACK sent... We could also piggyback data here")
+            # check if ack sent by server is my seq + 1
+            if his_ack == (initial_seq_num + 1):
+
+                # HANDSHAKE step 3, server sent SYN_ACK, send final ACK
+                # add 1 to client's seq_num
+                my_ack = his_seq_num + 1
+                # generate my own seq_num
+                my_seq_num = initial_seq_num + 1    # increment my seq num
+
+                # create ACK packet, ack will be in payload since we dont have ACK in the Packet data structure
+                my_dict_payload = {}
+                my_dict_payload['ack'] = my_ack
+                my_dict_payload['msg'] = ""
+                msg = json.dumps(my_dict_payload)
+
+                # create final ACK packet
+                ack_p = Packet(packet_type=2,
+                       seq_num=my_seq_num,
+                       peer_ip_addr=peer_ip,
+                       peer_port=server_port,
+                       payload=msg.encode("utf-8"))
+
+                print("sending ACK: ", my_ack, " SEQ:", my_seq_num)
+
+                conn.sendto(ack_p.to_bytes(), sender)
+                print("ACK sent... We could also piggyback data here")
+
+                ####### 3-way handshake good #########
+
+                # print('Router: ', sender)
+                # print('Packet: ', p)
+                # print('Payload: ' + p.payload.decode("utf-8"))
+                return True                
+
+            else:
+                # its someone else's SYN_ACK
+                print("ACK # didnt match my initial SEQ num. Redo handshake")
+                return False
 
         elif packet_type == NAK:
             print("NAK sent by server. CRASH AND BURN!!!")
-
-        print('Router: ', sender)
-        print('Packet: ', p)
-        print('Payload: ' + p.payload.decode("utf-8"))
-        return True
+            print ("Redo handshake")
+            return False
 
     except socket.timeout:
         print('No response after {}s'.format(timeout))
         print('Repeat handshake')
         return False
-    finally:
-        conn.close()
+    # finally:
+    #     conn.close()
 
 
 def run_client(router_addr, router_port, server_addr, server_port):
     peer_ip = ipaddress.ip_address(socket.gethostbyname(server_addr))
     conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    timeout = 5
-    try:
-        msg = "Hello World 123"
+    timeout = 10
+
+    ### implement handshake before sending data ###
+    while True:
+        if three_way_handshake(router_addr, router_port, server_addr, server_port, conn):
+            print("Cleint side: 3-way handshake successful"), conn
+            print("Sending HTTP Request")
+            break
+        else:   # loop to do three_way_handshake again
+            print("3-way handshake failed")
+
+
+    try:     
+        msg = "Put HTTP request here"
         p = Packet(packet_type=0,
                    seq_num=1,
                    peer_ip_addr=peer_ip,
                    peer_port=server_port,
                    payload=msg.encode("utf-8"))
 
-
-        ### implement handshake before sending data ###
-        if three_way_handshake(router_addr, router_port, server_addr, server_port):
-            print("3-way handshake successfull")
-        else:
-            print("3-way handshake failed")
-            sys.exit()
-
-
-        sys.exit()
-
-
+        # sys.exit()
 
         conn.sendto(p.to_bytes(), (router_addr, router_port))
         print('Send "{}" to router'.format(msg))
@@ -129,6 +165,7 @@ def run_client(router_addr, router_port, server_addr, server_port):
         print('Waiting for a response')
         response, sender = conn.recvfrom(1024)
         p = Packet.from_bytes(response)
+        print("===== HTTP Response ====")
         print('Router: ', sender)
         print('Packet: ', p)
         print('Payload: ' + p.payload.decode("utf-8"))
--- python/udp_server.py
+++ python/udp_server.py
@@ -1,5 +1,7 @@
 import argparse
 import socket
+import json
+import pprint
 
 from packet import Packet
 
@@ -9,6 +11,9 @@ SYN = 1
 ACK = 2
 SYN_ACK = 3
 NAK = -1
+
+
+three_way_handshake_good = False
 
 """
 1. Implement 3-way handshake before sending anything
@@ -38,47 +43,121 @@ def run_server(port):
         conn.close()
 
 
-def handle_client(conn, data, sender):
 
-    three_way_handshake = False
+def start_http(conn, data, sender, p):
 
     try:
         p = Packet.from_bytes(data)
 
         packet_type = p.packet_type
-        seq_num = p.seq_num
+        his_seq_num = p.seq_num
         peer_ip = p.peer_ip_addr
         peer_port = p.peer_port 
+        his_payload = p.payload.decode("utf-8")    # this is the HTTP request
 
+    except Exception as e:
+        print("Error: ", e)
+
+    msg = "HTTP Response, last client seq_num: " + str(his_seq_num)
+    syn_ack_p = Packet(packet_type=0,
+           seq_num=his_seq_num,
+           peer_ip_addr=peer_ip,
+           peer_port=peer_port,
+           payload=msg.encode("utf-8"))
+
+    conn.sendto(syn_ack_p.to_bytes(), sender)
+
+
+def handle_client(conn, data, sender):
+
+    global three_way_handshake_good
+
+    
+    # generate my own seq_num
+    initial_seq_num = 500    
+
+    try:
+        p = Packet.from_bytes(data)
+
+        packet_type = p.packet_type
+        his_seq_num = p.seq_num
+        peer_ip = p.peer_ip_addr
+        peer_port = p.peer_port 
+        his_payload = p.payload.decode("utf-8")
 
         # check packet type
-        if packet_type == DATA:
-            if three_way_handshake == False:
-                print("3-way handshake not completed. Data packet refused")
-            else:
-                print("Data accepted")
-        elif packet_type == SYN:
-            # 2nd step of handshake : if SYN, this is step 1 of handshake, add 1 to client's seq_num
-            my_ack = seq_num + 1
-            # generate my own seq_num
-            my_seq_num = 500
+        if packet_type == SYN:
 
-            # create SYN_ACK packet
-            msg = ""
+            print("received SYN with SEQ: ", his_seq_num)
+
+            # 2nd step of handshake : if SYN, this is step 1 of handshake, add 1 to client's seq_num
+            my_ack = his_seq_num + 1
+
+            # HANDSHAKE step 2
+            # create SYN_ACK packet, ack will be in payload since we dont have ACK in the Packet data structure
+            my_dict_payload = {}
+            my_dict_payload['ack'] = my_ack
+            my_dict_payload['msg'] = ""
+            msg = json.dumps(my_dict_payload)
+
             syn_ack_p = Packet(packet_type=3,
-                   seq_num=my_seq_num,
+                   seq_num=initial_seq_num,
                    peer_ip_addr=peer_ip,
                    peer_port=peer_port,
                    payload=msg.encode("utf-8"))
 
+            print("Sending SYN_ACK with SEQ: ", initial_seq_num, " ACK: ", my_ack)
+
             conn.sendto(syn_ack_p.to_bytes(), sender)
 
+            #### wait for final ACK for Handshake from Client
 
+        # receives an ACK
+        elif packet_type == ACK:  
+            # get ack from payload
+            dict_payload = json.loads(his_payload)
 
-        print("Router: ", sender)
-        print("Packet: ", p)
-        print("Payload: ", p.payload.decode("utf-8"))
-        print("Packet Type: ", packet_type)
+            try:
+
+                his_ack = dict_payload['ack']
+                print("received final handshake ACK: ", his_ack, " SEQ: ", his_seq_num)
+
+                # check if ack sent by client is my seq + 1
+                if his_ack == (initial_seq_num + 1):
+                    three_way_handshake_good = True
+                    print("Server side: 3 way handshake completed. Waiting for HTTP request...")
+
+                else:
+                    print("Wrong ACK number received")
+                    # wrong ACK number recieved, send NAK
+
+            except KeyError:
+                print("ACK not found in packet")
+
+                # keep waiting for correct ACK for handshake            
+
+        elif packet_type == DATA:
+            if three_way_handshake_good:
+                print("===== HTTP request ======")
+                print("Router: ", sender)
+                print("Packet: ", p)
+                print("Payload: ", p.payload.decode("utf-8"))
+                print("Packet Type: ", packet_type)
+
+                start_http(conn, data, sender, his_seq_num)
+
+            else:
+                print("3-way handshake not completed yet. Data packet refused")
+                
+
+        # if three_way_handshake_good:
+        #     print("Router: ", sender)
+        #     print("Packet: ", p)
+        #     print("Payload: ", p.payload.decode("utf-8"))
+        #     print("Packet Type: ", packet_type)
+
+        # else:
+        #     print("waiting for client to (re)start handshake")
 
         # How to send a reply.
         # The peer address of the packet p is the address of the client already.
